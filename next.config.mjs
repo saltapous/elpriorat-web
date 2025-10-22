36b9be14dddc1683f0005c7865abbd7605abbc1b
saltapous <albertdelfondo@gmail.com>
2025-10-22 17:37:16 +0200

    Config Next.js per GitHub Pages (export estàtic)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'elpriorat-web';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  output: 'export',                 // fa `next export`
+  images: { unoptimized: true },    // requerit a GitHub Pages
+  basePath: isProd ? `/${repo}` : '',// necessari en Project Pages
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true               // ajuda amb rutes estàtiques
 };
+
 export default nextConfig;
 
