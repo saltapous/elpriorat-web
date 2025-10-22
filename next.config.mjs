b64e61441ef9ba3cbee82bae70a6536a269f3c6e
saltapous <albertdelfondo@gmail.com>
2025-10-23 00:29:07 +0200

    fix: afegit fitxer CNAME i netejat basePath

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,13 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === 'production' ? '/elpriorat-web' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/elpriorat-web/' : '',
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
+
 
 
 
